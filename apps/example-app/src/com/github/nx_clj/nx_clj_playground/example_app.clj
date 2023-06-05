9d179a3b1868342ee03551cc4c8a66585839fc10
Caio Oliveira <caioaao@gmail.com>
2023-06-05 10:52:26 -0300

    use function from utils lib in example-app

--- apps/example-app/src/com/github/nx_clj/nx_clj_playground/example_app.clj
+++ apps/example-app/src/com/github/nx_clj/nx_clj_playground/example_app.clj
@@ -1,5 +1,6 @@
 (ns com.github.nx-clj.nx-clj-playground.example-app
+  (:require [com.github.nx-clj.nx-clj-playground.utils :as utils])
   (:gen-class))
 
-(defn -main [& _]
-  (println "Hello!"))
+(defn -main [name & _]
+  (println (utils/greet name)))
